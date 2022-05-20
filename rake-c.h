7fbcbf3d3101e3dbd3e469818a75feeb8385d753
Kat3434 <104178362+Kat3434@users.noreply.github.com>
2022-05-20 09:08:03 +0800

    Update rake-c.h

--- rake-c.h
+++ rake-c.h
@@ -6,9 +6,9 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 
 #define R_NO 100
